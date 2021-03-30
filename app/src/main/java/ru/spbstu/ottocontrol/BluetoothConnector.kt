3f405f5fcadf2797178ebe5073c0aaa7a3e3efa8
MaksimZotov <zotovmaksim1254@gmail.com>
2021-03-31 00:14:57 +0300

    implemented adding real devices to the dynamic list

--- app/src/main/java/ru/spbstu/ottocontrol/BluetoothConnector.kt
+++ app/src/main/java/ru/spbstu/ottocontrol/BluetoothConnector.kt
@@ -1,6 +1,12 @@
 package ru.spbstu.ottocontrol
 
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Parcelable
 import android.widget.Toast
 
 
@@ -8,25 +14,27 @@ class BluetoothConnector(val viewModel: MainActivityViewModel)  {
     lateinit var bluetoothAdapter: BluetoothAdapter
 
 
-    fun initBluetooth() {
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+    private val mReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            if (intent.action == BluetoothDevice.ACTION_FOUND) {
+                val device = intent.getParcelableExtra<Parcelable>(BluetoothDevice.EXTRA_DEVICE) as BluetoothDevice
+                viewModel.availableDevices.add("${device.name}; ${device.address}")
+                viewModel.notifyViewAboutStateChange()
+            }
+        }
     }
 
+
+    fun initBluetooth() { bluetoothAdapter = BluetoothAdapter.getDefaultAdapter() }
+
     fun searchAvailableDevices() {
-        viewModel.availableDevices.clear()
+        if (bluetoothAdapter.isDiscovering) bluetoothAdapter.cancelDiscovery()
 
         if (askForTurnBluetoothOn()) {
+            viewModel.availableDevices.clear()
+
+            viewModel.view.registerReceiver(mReceiver, IntentFilter(BluetoothDevice.ACTION_FOUND))
             bluetoothAdapter.startDiscovery()
-
-            // Добавляются только ранее подключенные устройства
-            // for (device in bluetoothAdapter.bondedDevices)
-            //     viewModel.availableDevices.add(device.name + "; " + device.address)
-
-            // Заглушка:
-            val numberOfDevices = (1..10).random()
-            for (i in 1..numberOfDevices) viewModel.availableDevices.add("device $i")
-
-            viewModel.notifyViewAboutStateChange()
         }
     }
 
