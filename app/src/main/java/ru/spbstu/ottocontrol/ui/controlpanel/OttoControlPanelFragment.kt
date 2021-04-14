94b152d24a77e5fe16ba303c39d1ea07d85d57b2
Maxim Moschenskikh <m.moschenskikh@gmail.com>
2021-04-14 20:17:51 +0300

    Stubs for each fragment

---
+++ app/src/main/java/ru/spbstu/ottocontrol/ui/controlpanel/OttoControlPanelFragment.kt
@@ -0,0 +1,9 @@
+package ru.spbstu.ottocontrol.ui.controlpanel
+
+import ru.spbstu.ottocontrol.databinding.FragmentOttoControlPanelBinding
+import ru.spbstu.ottocontrol.ui.base.BaseFragment
+
+class OttoControlPanelFragment :
+    BaseFragment<FragmentOttoControlPanelBinding>(FragmentOttoControlPanelBinding::inflate) {
+    // TODO()
+}
